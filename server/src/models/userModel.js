ccdaafbfcc45978c7bdc8b32c80883e53b5e0ac2
Mukta <sucessdy@gmail.com>
2026-07-30 14:52:54 +0530

    refactor the client code

--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -38,7 +38,7 @@ const userSchema = new mongoose.Schema(
   },
 );
 
-userSchema.pre("save", async function (next) {
+userSchema.pre("save", async function () {
   if (!this.isModified("password")) return;
   this.password = await bcrypt.hash(this.password, 10);
 });
